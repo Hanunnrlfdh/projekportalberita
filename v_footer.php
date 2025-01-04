b424c4d00fd6ee71ab7af882197cd2e63a4b4564
Hanunnrlfdh <hanun.nurul18@gmail.com>
2025-01-04 19:17:24 +0700

    Add files via upload

---
+++ v_footer.php
@@ -0,0 +1,44 @@
+<footer id="fh5co-footer" role="contentinfo">
+
+		<div class="container">
+			<div class="col-md-3 col-sm-9 col-sm-push-0 ">
+				<h3>Portal Berita</h3>
+				<p>Portal Berita adalah situs web yang menyediakan informasi yang terjadi di STMIK MARDIRA INDONESIA </p>
+			</div>
+			<div class="col-md-4 col-md-push-1 col-sm-9 col-sm-push-0 col-xs-9 col-xs-push-0">
+				<h3>STMIK MARDIRA INDONESIA</h3>
+				<ul class="float">
+					<li><a href="<?php echo base_url().'portfolio'?>">STMIK MARDIRA INDONESIA</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">HANUN NURUL FADIAH</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">IFSI-S1/a></li>
+				</ul>
+				
+
+			</div>
+			<div class="col-md-2 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>INFORMASI BERITA</h3>
+				<ul class="float">
+					<li><a href="<?php echo base_url().'portfolio'?>">STMIK MARDIRA INDONESIA</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">BEC</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">BCC</a></li>
+					<li><a href="<?php echo base_url().'portfolio'?>">PKM</a></li>
+				</ul>
+			</div>
+
+			<div class="col-md-2 col-md-push-1 col-sm-12 col-sm-push-0 col-xs-12 col-xs-push-0">
+				<h3>Sosial Media</h3>
+				<ul class="fh5co-social">
+					<li><a href="#"><i class="icon-twitter"></i></a></li>
+					<li><a href="#"><i class="icon-facebook"></i></a></li>
+					<li><a href="#"><i class="icon-google-plus"></i></a></li>
+					<li><a href="#"><i class="icon-instagram"></i></a></li>
+				</ul>
+			</div>
+
+
+			<div class="col-md-12 fh5co-copyright text-center">
+				<p>&copy; 2024 by Hanun Nurul</a>. All Rights Reserved.</p>
+			</div>
+
+		</div>
+	</footer>
